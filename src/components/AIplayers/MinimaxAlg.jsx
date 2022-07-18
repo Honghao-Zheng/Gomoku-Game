87804288f5799e2b6807620910cff51b3d70ecc2
Honghao Zheng <hxz157@cs.bham.ac.uk>
2022-07-18 03:05:01 +0100

    minimax recursive done for any depth no testing

--- src/components/AIplayers/MinimaxAlg.jsx
+++ src/components/AIplayers/MinimaxAlg.jsx
@@ -58,9 +58,9 @@ function minimaxMoveBad(turn,depthAwayFromRoot=0,depthAwayFromLeaf=4,board,alpha
                     break;
                 }
             }
-            //assume there is at least one B2 board, selecting the minimum B2 score.
+
         }
-        //compute the minimum score picked by the opponent, the minimum score will be the score of B1
+
 
         B1Score=bestScore2;
         if(B1Score>=bestScore1){
@@ -83,8 +83,9 @@ function minimaxMoveBad(turn,depthAwayFromRoot=0,depthAwayFromLeaf=4,board,alpha
     }
     
 }
-function minimaxMove(turn,depthAwayFromRoot=0,depthAwayFromLeaf=4,board,alphaScore){
-    depthAwayFromLeaf-=2;
+function minimaxMove(turn,depthAwayFromRoot,depthAwayFromLeaf,board,bestScore1=-100000,alphaBetaScore=100000){
+    depthAwayFromLeaf-=1;
+    depthAwayFromRoot+=1
     let defFactor=0.8;
     let branchFactor=5
     let opponentColor=swapColor(turn)
@@ -95,60 +96,61 @@ function minimaxMove(turn,depthAwayFromRoot=0,depthAwayFromLeaf=4,board,alphaSco
     let moveMade2;
     let B1Score;
     let B2Score;
-    let bestScore1;
     let bestScore2;
     let avalibleMoves2;
     let bestMove;
     let B1;
     let B2;
     
-    bestScore1=-100000;
     //first depth, consider different computer moves
     for (moveIndex1=0;moveIndex1<avalibleMoves1.length;moveIndex1++){
         B1=copyTwoDimArray(board)
         moveMade1=avalibleMoves1[moveIndex1].move
         putDownPiece(moveMade1,turn,B1)
-        avalibleMoves2=movesSearch(turn,defFactor,B1,branchFactor)
-        bestScore2=100000;
-        //second depth, consider different opponent moves
-        for (moveIndex2=0;moveIndex2<avalibleMoves2.length;moveIndex2++){
-            B2=copyTwoDimArray(B1)
-            moveMade2=avalibleMoves2[moveIndex2].move
-            putDownPiece(moveMade2,opponentColor,B2)
-            //evaluate score for move once it reached the leaf node
-            if(depthAwayFromLeaf===0){
-                B2Score=-moveEvaluation(moveMade2,opponentColor,defFactor,B2).score          
-            } else {
-                depthAwayFromRoot+=2
-                B2Score=minimaxMove(turn,depthAwayFromRoot,depthAwayFromLeaf,B2,bestScore2)
-                depthAwayFromRoot-=2
-            }
+        //alphabeta score is the default score for 
 
-            if(B2Score<=bestScore2){
-                bestScore2=B2Score
-                if(B2Score<=bestScore1){
-                    break;
-                }
+        //bestScore1 is the default score of current parent node
+
+        //bestScore2 is the default score of parent node for next branching out
+        bestScore2=-bestScore1;
+
+        if(depthAwayFromLeaf===0){
+            if(depthAwayFromRoot%2===1){
+                B1Score=moveEvaluation(moveMade1,turn,defFactor,B1).score   
+            } else{
+                B1Score=-moveEvaluation(moveMade1,turn,defFactor,B1).score  
             }
-            //assume there is at least one B2 board, selecting the minimum B2 score.
+            
+        } else {
+            B1Score=minimaxMove(opponentColor,depthAwayFromRoot,depthAwayFromLeaf,B1,bestScore2,bestScore1)
+            depthAwayFromRoot-=1
         }
-        //compute the minimum score picked by the opponent, the minimum score will be the score of B1
-
-        B1Score=bestScore2;
-        if(B1Score>=bestScore1){
-            bestScore1=B1Score;
-            bestMove=moveMade1;
-            if(depthAwayFromRoot !==0  ){
-                if(B1Score>=alphaScore){
+        //odd depth maximiser
+        if(depthAwayFromRoot%2===1){
+            if(B1Score>=bestScore1){
+                bestScore1=B1Score;
+                bestMove=moveMade1;
+                if(B1Score>=alphaBetaScore){
                     break;
                 }
             }
-        }    
+        // even depth minimiser
+        } else {
+            if(B1Score<=bestScore1){
+                bestScore1=B1Score
+                bestMove=moveMade1;
+                if(B1Score<=alphaBetaScore){
+                    break;
+                }
+            }
+        }
+
+          
     }
-    //return the score back to the parent node
-    if(depthAwayFromRoot !==0  ){
+    //return the score back to the parent node if it is not in the first depth
+    if(depthAwayFromRoot !==1  ){
         return bestScore1;
-    //if already in the first two depth, return the move.
+    //if already in the first depth, return the move.
     } else {
         console.log("bestMove: "+bestMove)
         return bestMove;
